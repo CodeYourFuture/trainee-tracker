6a218750bd00f051d4ce3fde9d4d912ffa76b541
Daniel Wagner-Hall <dawagner@gmail.com>
2025-08-14 13:13:40 +0100

    Some SDC modules have 4 sprints

--- src/bin/pr-metadata-validator.rs
+++ src/bin/pr-metadata-validator.rs
@@ -163,7 +163,8 @@ fn make_fake_course_schedule(module_name: String) -> CourseSchedule {
     sprints.insert(
         module_name,
         std::iter::repeat_with(|| btreemap![ARBITRARY_REGION => fixed_date])
-            .take(3)
+            // 4 is the max number of sprints a module (currently) contains.
+            .take(4)
             .collect(),
     );
     CourseSchedule {
