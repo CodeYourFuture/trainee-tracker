CodeYourFuture/trainee-tracker

commit aa0d5787598ab8191f6429b6858f3b62dd2a6f9f

Switch from sheets to google-sheets4 (#36)
This API preserves the `Option`ality of returned fields, which is useful
in determining the type of a field.

Along with this, centralise parsing of sheet data into the get function,
rather than pushing it on all callers.

src/register.rs
@@ -1,9 +1,9 @@
 use anyhow::Context;
 use chrono::{DateTime, NaiveDate, Utc};
 use email_address::EmailAddress;
+use google_sheets4::api::CellData;
 use indexmap::IndexMap;
 use serde::Serialize;
-use sheets::types::{CellData, GridData};
 use tracing::warn;
 
 use crate::{
@@ -61,124 +61,100 @@ pub(crate) async fn get_register(
 ) -> Result<Register, Error> {
     let mut modules: IndexMap<String, ModuleAttendance> = IndexMap::new();
 
-    let data = client
-        .get(&register_sheet_id, true, &[])
-        .await
-        .map_err(|err| {
-            err.with_context(|| format!("Failed to get spreadsheet with ID {}", register_sheet_id))
-        })?;
-    for sheet in data.body.sheets {
-        if let Some(properties) = &sheet.properties {
-            let title = properties.title.clone();
-            if modules.contains_key(&title) {
-                return Err(Error::Fatal(anyhow::anyhow!(
-                    "Failed to read register sheet ID {} - duplicate sheets {}",
-                    register_sheet_id,
-                    title
-                )));
-            }
-            let register_url = format!(
-                "{}{}gid={}",
-                data.body.spreadsheet_url,
-                if data.body.spreadsheet_url.contains("?") {
-                    "&"
-                } else {
-                    "?"
-                },
-                properties.sheet_id
-            );
-            let attendance = read_module(sheet.data, register_url.clone(), start_date, end_date)
-                .with_context(|| {
-                    format!(
-                        "Failed to read register sheet ID {} sheet {}",
-                        register_sheet_id, title
-                    )
-                })?;
-            let module = ModuleAttendance {
-                register_url,
-                attendance,
-            };
-            // TODO: Unify module names across sources (repo has Module-prefix, register does not)
-            modules.insert(format!("Module-{}", title.replace(' ', "-")), module);
-        } else {
-            warn!("Ignoring sheet in {} with no properties", register_sheet_id);
+    let data = client.get(&register_sheet_id).await.map_err(|err| {
+        err.with_context(|| format!("Failed to get spreadsheet with ID {}", register_sheet_id))
+    })?;
+    for (title, sheet) in data.into_iter() {
+        if modules.contains_key(&title) {
+            return Err(Error::Fatal(anyhow::anyhow!(
+                "Failed to read register sheet ID {} - duplicate sheets {}",
+                register_sheet_id,
+                title
+            )));
         }
+        let register_url = format!(
+            "{}{}gid={}",
+            sheet.url,
+            if sheet.url.contains("?") { "&" } else { "?" },
+            sheet.id
+        );
+        let attendance = read_module(sheet.rows, register_url.clone(), start_date, end_date)
+            .with_context(|| {
+                format!(
+                    "Failed to read register sheet ID {} sheet {}",
+                    register_sheet_id, title
+                )
+            })?;
+        let module = ModuleAttendance {
+            register_url,
+            attendance,
+        };
+        // TODO: Unify module names across sources (repo has Module-prefix, register does not)
+        modules.insert(format!("Module-{}", title.replace(' ', "-")), module);
     }
     Ok(Register { modules })
 }
 
 fn read_module(
-    sheet_data: Vec<GridData>,
+    sheet_data: Vec<Vec<CellData>>,
     register_url: String,
     start_date: NaiveDate,
     end_date: NaiveDate,
 ) -> Result<Vec<IndexMap<EmailAddress, Attendance>>, anyhow::Error> {
     let mut sprints = Vec::new();
-    'sheet: for data in sheet_data {
-        if data.start_column != 0 || data.start_row != 0 {
+    for (row_number, cells) in sheet_data.into_iter().enumerate() {
+        // Some sheets have documentation or pivot table
+        if row_number == 0 && !cells.is_empty() && cell_string(&cells[0]) != "Name" {
+            continue;
+        }
+        if cells.len() < 7 {
             return Err(anyhow::anyhow!(
-                "Start column and row were {} and {}, expected 0 and 0",
-                data.start_column,
-                data.start_row
+                "Not enough columns for row {} - expected at least 7, got {} containing: {}",
+                row_number,
+                cells.len(),
+                format!("{:#?}", cells),
             ));
         }
-        for (row_number, row) in data.row_data.into_iter().enumerate() {
-            let cells = row.values;
-            // Some sheets have documentation or pivot table
-            if row_number == 0 && !cells.is_empty() && cell_string(&cells[0]) != "Name" {
-                continue 'sheet;
-            }
-            if cells.len() < 7 {
+        if row_number == 0 {
+            let headings = cells.iter().take(7).map(cell_string).collect::<Vec<_>>();
+            if headings
+                != [
+                    "Name",
+                    "Email",
+                    "Timestamp",
+                    "Course",
+                    "Module",
+                    "Day",
+                    "Location",
+                ]
+            {
                 return Err(anyhow::anyhow!(
-                    "Not enough columns for row {} - expected at least 7, got {} containing: {}",
-                    row_number,
-                    cells.len(),
-                    format!("{:#?}", cells),
+                    "Register sheet contained wrong headings: {}",
+                    headings.join(", ")
                 ));
             }
-            if row_number == 0 {
-                let headings = cells.iter().take(7).map(cell_string).collect::<Vec<_>>();
-                if headings
-                    != [
-                        "Name",
-                        "Email",
-                        "Timestamp",
-                        "Course",
-                        "Module",
-                        "Day",
-                        "Location",
-                    ]
-                {
-                    return Err(anyhow::anyhow!(
-                        "Register sheet contained wrong headings: {}",
-                        headings.join(", ")
-                    ));
-                }
+        } else {
+            if cells[0].effective_value.is_none() {
+                break;
+            }
+            let (sprint_number, attendance) = read_row(&cells, register_url.clone())
+                .with_context(|| format!("Failed to read attendance from row {}", row_number))?;
+            if attendance.timestamp.date_naive() <= start_date
+                || attendance.timestamp.date_naive() >= end_date
+            {
+                continue;
+            }
+            let sprint_index = sprint_number - 1;
+            while sprints.len() < sprint_number {
+                sprints.push(IndexMap::new());
+            }
+            if sprints[sprint_index].contains_key(&attendance.email) {
+                warn!(
+                    "Register sheet contained duplicate entry for sprint {} trainee {}",
+                    sprint_number, attendance.email
+                );
             } else {
-                if cells[0].effective_value.is_none() {
-                    break;
-                }
-                let (sprint_number, attendance) = read_row(&cells, register_url.clone())
-                    .with_context(|| {
-                        format!("Failed to read attendance from row {}", row_number)
-                    })?;
-                if attendance.timestamp.date_naive() <= start_date
-                    || attendance.timestamp.date_naive() >= end_date
-                {
-                    continue;
-                }
-                let sprint_index = sprint_number - 1;
-                while sprints.len() < sprint_number {
-                    sprints.push(IndexMap::new());
-                }
-                if sprints[sprint_index].contains_key(&attendance.email) {
-                    warn!(
-                        "Register sheet contained duplicate entry for sprint {} trainee {}",
-                        sprint_number, attendance.email
-                    );
-                } else {
-                    sprints[sprint_index].insert(attendance.email.clone(), attendance);
-                }
+                sprints[sprint_index].insert(attendance.email.clone(), attendance);
             }
         }
     }

Cargo.toml
@@ -16,6 +16,7 @@ dotenv = "0.15.0"
 email_address = { git = "https://github.com/illicitonion/rust-email_address.git", rev = "12cd9762a166b79a227beaa90b2f60a768d7c55c" }
 futures = "0.3.31"
 google-drive = "0.7.0"
+google-sheets4 = "6.0.0"
 gsuite-api = "0.7.0"
 http = "1.3.1"
 http-serde = "2.1.1"

src/auth.rs
@@ -6,7 +6,6 @@ use axum::{
 };
 use http::Uri;
 use serde::Deserialize;
-use sheets::Client;
 use tower_sessions::Session;
 use uuid::Uuid;
 
@@ -112,7 +111,8 @@ pub async fn handle_google_oauth_callback(
         server_state.config.public_base_url
     );
 
-    let mut client = Client::new(
+    // TODO: Replace this with some other way of getting the token (either a request ourselves, or using another library) so we can drop the sheets dep.
+    let mut client = ::sheets::Client::new(
         server_state.config.google_apis_client_id.clone(),
         (*server_state.config.google_apis_client_secret).clone(),
         redirect_uri,

src/github_accounts.rs
@@ -3,12 +3,11 @@ use std::collections::BTreeMap;
 use anyhow::Context;
 use email_address::EmailAddress;
 use serde::{Deserialize, Serialize};
-use sheets::types::Sheet;
 
 use crate::{
     Error,
     newtypes::{GithubLogin, Region, new_case_insensitive_email_address},
-    sheets::{SheetsClient, cell_string},
+    sheets::{Sheet, SheetsClient, cell_string},
 };
 
 // TODO: Replace this with a serde implementation from a Google Sheet.
@@ -17,7 +16,7 @@ pub(crate) async fn get_trainees(
     sheet_id: &str,
 ) -> Result<BTreeMap<GithubLogin, Trainee>, Error> {
     const EXPECTED_SHEET_NAME: &str = "Form responses 1";
-    let data = client.get(sheet_id, true, &[]).await.map_err(|err| {
+    let data = client.get(sheet_id).await.map_err(|err| {
         err.with_context(|| {
             format!(
                 "Failed to get trainees github accounts sheet with id {}",
@@ -25,24 +24,11 @@ pub(crate) async fn get_trainees(
             )
         })
     })?;
-    let sheet = data.body.sheets.into_iter().find(|sheet| {
-        if let Some(properties) = &sheet.properties {
-            properties.title == EXPECTED_SHEET_NAME
-        } else {
-            false
-        }
-    });
+    let sheet = data.get(EXPECTED_SHEET_NAME);
     if let Some(sheet) = sheet {
         let data = trainees_from_sheet(&sheet).map_err(|err| {
             err.with_context(|| {
-                format!(
-                    "Failed to read trainees from sheet {}",
-                    sheet
-                        .properties
-                        .map(|properties| properties.title)
-                        .as_deref()
-                        .unwrap_or("<unknown>")
-                )
+                format!("Failed to read trainees from sheet {}", EXPECTED_SHEET_NAME,)
             })
         })?;
         Ok(data)
@@ -65,41 +51,31 @@ pub struct Trainee {
 
 fn trainees_from_sheet(sheet: &Sheet) -> Result<BTreeMap<GithubLogin, Trainee>, Error> {
     let mut trainees = BTreeMap::new();
-    for data in &sheet.data {
-        if data.start_column != 0 || data.start_row != 0 {
+    for (row_index, cells) in sheet.rows.iter().enumerate() {
+        if row_index == 0 {
+            continue;
+        }
+        if cells.len() < 5 {
             return Err(Error::Fatal(anyhow::anyhow!(
-                "Reading data from Google Sheets API - got data chunk that didn't start at row=0,column=0 - got row={},column={}",
-                data.start_row,
-                data.start_column
+                "Reading trainee data from Google Sheets API, row {} didn't have at least 5 columns",
+                row_index
             )));
         }
-        for (row_index, row) in data.row_data.iter().enumerate() {
-            if row_index == 0 {
-                continue;
-            }
-            let cells = &row.values;
-            if cells.len() < 5 {
-                return Err(Error::Fatal(anyhow::anyhow!(
-                    "Reading trainee data from Google Sheets API, row {} didn't have at least 5 columns",
-                    row_index
-                )));
-            }
 
-            let github_login = GithubLogin::from(cell_string(&cells[3]));
+        let github_login = GithubLogin::from(cell_string(&cells[3]));
 
-            let email = cell_string(&cells[4]);
+        let email = cell_string(&cells[4]);
 
-            trainees.insert(
-                github_login.clone(),
-                Trainee {
-                    name: cell_string(&cells[1]),
-                    region: Region(cell_string(&cells[2])),
-                    github_login,
-                    email: new_case_insensitive_email_address(&email)
-                        .with_context(|| format!("Failed to parse trainee email {}", email))?,
-                },
-            );
-        }
+        trainees.insert(
+            github_login.clone(),
+            Trainee {
+                name: cell_string(&cells[1]),
+                region: Region(cell_string(&cells[2])),
+                github_login,
+                email: new_case_insensitive_email_address(&email)
+                    .with_context(|| format!("Failed to parse trainee email {}", email))?,
+            },
+        );
     }
 
     Ok(trainees)

src/mentoring.rs
@@ -2,8 +2,8 @@ use std::collections::{BTreeMap, btree_map::Entry};
 
 use anyhow::Context;
 use chrono::{NaiveDate, Utc};
+use google_sheets4::api::CellData;
 use serde::Serialize;
-use sheets::types::GridData;
 use tracing::warn;
 
 use crate::{
@@ -44,54 +44,48 @@ pub async fn get_mentoring_records(
         records: BTreeMap::new(),
     };
 
-    for sheet_data in sheet_data {
-        if sheet_data.start_column != 0 || sheet_data.start_row != 0 {
-            return Err(Error::Fatal(anyhow::anyhow!(
-                "Start column and row were {} and {}, expected 0 and 0",
-                sheet_data.start_column,
-                sheet_data.start_row
-            )));
+    for (row_number, cells) in sheet_data.into_iter().enumerate() {
+        if cells.is_empty() {
+            continue;
         }
-
-        for (row_number, row) in sheet_data.row_data.into_iter().enumerate() {
-            let cells = row.values;
-            if cells.is_empty() {
-                continue;
+        if cells.len() < 6 && !cell_string(&cells[0]).is_empty() {
+            warn!(
+                "Parsing mentoring data from Google Sheet with ID {}: Not enough columns for row {} - expected at least 6, got {} containing: {}",
+                mentoring_records_sheet_id,
+                row_number,
+                cells.len(),
+                format!("{:#?}", cells),
+            );
+            continue;
+        }
+        if row_number == 0 {
+            let headings = cells.iter().take(6).map(cell_string).collect::<Vec<_>>();
+            if headings != ["Name", "Region", "Date", "Staff", "Status", "Notes"] {
+                return Err(Error::Fatal(anyhow::anyhow!(
+                    "Mentoring data sheet contained wrong headings: {}",
+                    headings.join(", ")
+                )));
             }
-            if cells.len() < 6 && !cell_string(&cells[0]).is_empty() {
-                warn!(
-                    "Parsing mentoring data from Google Sheet with ID {}: Not enough columns for row {} - expected at least 6, got {} containing: {}",
-                    mentoring_records_sheet_id,
-                    row_number,
-                    cells.len(),
-                    format!("{:#?}", cells),
-                );
-                continue;
+        } else {
+            if cells[0].effective_value.is_none() {
+                break;
             }
-            if row_number == 0 {
-                let headings = cells.iter().take(6).map(cell_string).collect::<Vec<_>>();
-                if headings != ["Name", "Region", "Date", "Staff", "Status", "Notes"] {
-                    return Err(Error::Fatal(anyhow::anyhow!(
-                        "Mentoring data sheet contained wrong headings: {}",
-                        headings.join(", ")
-                    )));
+            let name = cell_string(&cells[0]);
+            let date = cell_date(&cells[2]).with_context(|| {
+                format!(
+                    "Failed to parse date from row {} in sheet ID {}",
+                    row_number + 1,
+                    mentoring_records_sheet_id
+                )
+            })?;
+            let entry = mentoring_records.records.entry(name);
+            match entry {
+                Entry::Vacant(entry) => {
+                    entry.insert(MentoringRecord { last_date: date });
                 }
-            } else {
-                if cells[0].effective_value.is_none() {
-                    break;
-                }
-                let name = cell_string(&cells[0]);
-                let date = cell_date(&cells[2])
-                    .with_context(|| format!("Failed to parse date from row {}", row_number + 1))?;
-                let entry = mentoring_records.records.entry(name);
-                match entry {
-                    Entry::Vacant(entry) => {
+                Entry::Occupied(mut entry) => {
+                    if entry.get().last_date < date {
                         entry.insert(MentoringRecord { last_date: date });
-                    }
-                    Entry::Occupied(mut entry) => {
-                        if entry.get().last_date < date {
-                            entry.insert(MentoringRecord { last_date: date });
-                        }
                     }
                 }
             }
@@ -103,9 +97,10 @@ pub async fn get_mentoring_records(
 async fn get_mentoring_records_grid_data(
     client: SheetsClient,
     mentoring_records_sheet_id: &str,
-) -> Result<Vec<GridData>, Error> {
-    let data_result = client.get(mentoring_records_sheet_id, true, &[]).await;
-    let data = match data_result {
+) -> Result<Vec<Vec<CellData>>, Error> {
+    let expected_sheet_title = "Feedback";
+    let data_result = client.get(mentoring_records_sheet_id).await;
+    let mut data = match data_result {
         Ok(data) => data,
         Err(Error::PotentiallyIgnorablePermissions(_)) => {
             return Ok(Vec::new());
@@ -120,24 +115,12 @@ async fn get_mentoring_records_grid_data(
             return Err(err);
         }
     };
-    let expected_sheet_title = "Feedback";
-    let sheet = data
-        .body
-        .sheets
-        .into_iter()
-        .find(|sheet| {
-            sheet
-                .properties
-                .as_ref()
-                .map(|properties| properties.title.as_str())
-                == Some(expected_sheet_title)
-        })
-        .ok_or_else(|| {
-            Error::Fatal(anyhow::anyhow!(
-                "Couldn't find sheet '{}' in spreadsheet with ID {}",
-                expected_sheet_title,
-                mentoring_records_sheet_id
-            ))
-        })?;
-    Ok(sheet.data)
+    let sheet = data.remove(expected_sheet_title).ok_or_else(|| {
+        Error::Fatal(anyhow::anyhow!(
+            "Couldn't find sheet '{}' in spreadsheet with ID {}",
+            expected_sheet_title,
+            mentoring_records_sheet_id
+        ))
+    })?;
+    Ok(sheet.rows)
 }

src/prs.rs
@@ -207,14 +207,14 @@ pub(crate) async fn fill_in_reviewers(
         .collect())
 }
 
-#[derive(PartialEq, Eq, Serialize)]
+#[derive(Debug, PartialEq, Eq, Serialize)]
 pub(crate) enum CheckStatus {
     CheckedAndOk,
     CheckedAndCheckAgain,
     Unchecked,
 }
 
-#[derive(PartialEq, Eq, Serialize)]
+#[derive(Debug, PartialEq, Eq, Serialize)]
 pub(crate) struct ReviewerStaffOnlyDetails {
     pub(crate) name: String,
     pub(crate) attended_training: bool,

src/reviewer_staff_info.rs
@@ -1,12 +1,10 @@
 use std::collections::BTreeMap;
 
-use sheets::types::Sheet;
-
 use crate::{
     Error,
     newtypes::GithubLogin,
     prs::{CheckStatus, ReviewerStaffOnlyDetails},
-    sheets::{SheetsClient, cell_bool, cell_string},
+    sheets::{Sheet, SheetsClient, cell_bool, cell_string},
 };
 
 pub(crate) async fn get_reviewer_staff_info(
@@ -14,7 +12,7 @@ pub(crate) async fn get_reviewer_staff_info(
     sheet_id: &str,
 ) -> Result<BTreeMap<GithubLogin, ReviewerStaffOnlyDetails>, Error> {
     const EXPECTED_SHEET_NAME: &str = "Sheet1";
-    let data = client.get(sheet_id, true, &[]).await.map_err(|err| {
+    let mut data = client.get(sheet_id).await.map_err(|err| {
         err.with_context(|| {
             format!(
                 "Failed to get reviewer staff detail sheet with id {}",
@@ -22,23 +20,13 @@ pub(crate) async fn get_reviewer_staff_info(
             )
         })
     })?;
-    let sheet = data.body.sheets.into_iter().find(|sheet| {
-        if let Some(properties) = &sheet.properties {
-            properties.title == EXPECTED_SHEET_NAME
-        } else {
-            false
-        }
-    });
+    let sheet = data.remove(EXPECTED_SHEET_NAME);
     if let Some(sheet) = sheet {
         let data = reviewer_staff_detail_from_sheet(&sheet).map_err(|err| {
             err.with_context(|| {
                 format!(
                     "Failed to read reviewer staff details from sheet {}",
-                    sheet
-                        .properties
-                        .map(|properties| properties.title)
-                        .as_deref()
-                        .unwrap_or("<unknown>")
+                    EXPECTED_SHEET_NAME
                 )
             })
         })?;
@@ -56,47 +44,38 @@ fn reviewer_staff_detail_from_sheet(
     sheet: &Sheet,
 ) -> Result<BTreeMap<GithubLogin, ReviewerStaffOnlyDetails>, Error> {
     let mut reviewers = BTreeMap::new();
-    for data in &sheet.data {
-        if data.start_column != 0 || data.start_row != 0 {
-            return Err(Error::Fatal(anyhow::anyhow!(
-                "Reading data from Google Sheets API - got data chunk that didn't start at row=0,column=0 - got row={},column={}",
-                data.start_row,
-                data.start_column
-            )));
+
+    for (row_index, cells) in sheet.rows.iter().enumerate() {
+        if row_index == 0 {
+            continue;
         }
-        for (row_index, row) in data.row_data.iter().enumerate() {
-            if row_index == 0 {
-                continue;
-            }
-            let cells = &row.values;
-            if cells.len() < 6 {
-                continue;
-            }
-
-            let github_login = GithubLogin::from(cell_string(&cells[0]));
-
-            let notes = match cells.get(6) {
-                Some(cell) => cell_string(cell),
-                None => String::new(),
-            };
-
-            let checked = match (cell_bool(&cells[3]), cell_bool(&cells[4])) {
-                (true, false) => CheckStatus::CheckedAndOk,
-                (true, true) => CheckStatus::CheckedAndCheckAgain,
-                (false, _) => CheckStatus::Unchecked,
-            };
-
-            reviewers.insert(
-                github_login.clone(),
-                ReviewerStaffOnlyDetails {
-                    name: cell_string(&cells[1]),
-                    attended_training: cell_bool(&cells[2]),
-                    checked,
-                    quality: cell_string(&cells[5]),
-                    notes,
-                },
-            );
+        if cells.len() < 6 {
+            continue;
         }
+
+        let github_login = GithubLogin::from(cell_string(&cells[0]));
+
+        let notes = match cells.get(6) {
+            Some(cell) => cell_string(cell),
+            None => String::new(),
+        };
+
+        let checked = match (cell_bool(&cells[3]), cell_bool(&cells[4])) {
+            (true, false) => CheckStatus::CheckedAndOk,
+            (true, true) => CheckStatus::CheckedAndCheckAgain,
+            (false, _) => CheckStatus::Unchecked,
+        };
+
+        reviewers.insert(
+            github_login.clone(),
+            ReviewerStaffOnlyDetails {
+                name: cell_string(&cells[1]),
+                attended_training: cell_bool(&cells[2]),
+                checked,
+                quality: cell_string(&cells[5]),
+                notes,
+            },
+        );
     }
 
     Ok(reviewers)

commit 251153fa75bff5fd309032339922ac1d55d09acc

Fix attendance sheet filtering
This bug was introduced in aa0d5787598ab8191f6429b6858f3b62dd2a6f9f - it
used to continue out of an outer loop to the next sheet, but now the
continue just skips to the next row.

This function now handles just one sheet, so if a sheet is not a
register sheet, just return immediately.

src/register.rs
@@ -105,7 +105,7 @@ fn read_module(
     for (row_number, cells) in sheet_data.into_iter().enumerate() {
         // Some sheets have documentation or pivot table
         if row_number == 0 && !cells.is_empty() && cell_string(&cells[0]) != "Name" {
-            continue;
+            return Ok(sprints);
         }
         if cells.len() < 7 {
             return Err(anyhow::anyhow!(