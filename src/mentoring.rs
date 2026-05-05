7f9670f720e0ba63784c4842493450a4ef1247d5
Daniel Wagner-Hall <daniel@codeyourfuture.io>
2026-05-05 14:20:13 +0100

    Set up ITP May (#72)
    Also update a spreadsheet schema

--- src/mentoring.rs
+++ src/mentoring.rs
@@ -59,19 +59,29 @@ pub async fn get_mentoring_records(
             continue;
         }
         if row_number == 0 {
-            let headings = cells.iter().take(6).map(cell_string).collect::<Vec<_>>();
-            if headings != ["Name", "Region", "Date", "Staff", "Status", "Notes"] {
+            let headings = cells.iter().take(7).map(cell_string).collect::<Vec<_>>();
+            if headings
+                != [
+                    "Repeated in Check in Form",
+                    "Name",
+                    "Region",
+                    "Date",
+                    "Staff",
+                    "Status",
+                    "Notes",
+                ]
+            {
                 return Err(Error::Fatal(anyhow::anyhow!(
                     "Mentoring data sheet contained wrong headings: {}",
                     headings.join(", ")
                 )));
             }
         } else {
-            if cells[0].effective_value.is_none() {
+            if cells[1].effective_value.is_none() {
                 break;
             }
-            let name = cell_string(&cells[0]);
-            let date = cell_date(&cells[2]).with_context(|| {
+            let name = cell_string(&cells[1]);
+            let date = cell_date(&cells[3]).with_context(|| {
                 format!(
                     "Failed to parse date from row {} in sheet ID {}",
                     row_number + 1,
